48e798a23a41a7f30ab155fb5f8eb8c8d5c5ba33
Nick Satterly <nick.satterly@theguardian.com>
2017-08-22 09:35:13 +0100

    Free memory allocated to curl HTTP header list

--- src/alerta-neb.c
+++ src/alerta-neb.c
@@ -239,6 +239,7 @@ send_to_alerta(char *url, char *message)
   curl_easy_setopt (curl, CURLOPT_HTTPHEADER, headers);
   curl_easy_setopt (curl, CURLOPT_POSTFIELDS, message_mod);
   res = curl_easy_perform (curl);
+  curl_slist_free_all(headers);
 
   if (res != CURLE_OK) {
     sprintf (message, "[alerta] curl_easy_perform() failed: %s", curl_easy_strerror (res));
